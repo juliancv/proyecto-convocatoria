92b120f8335d26834c7734de17ca5650a401652c
Anderson Enriquez <anderson.enriquez@correounivalle.edu.co>
2014-10-22 09:59:34 -0500

    setters y getters para usuarios

--- src/logica/Usuario.java
+++ src/logica/Usuario.java
@@ -16,12 +16,13 @@ public class Usuario {
     private String apellidoDos;
     private String username;
     private String password;
+    private String tipoIdentificacion;
     private String identificacion;
     private boolean estado;
     private boolean empleo;
 
     public Usuario(String nombre1, String nombre2, String apellido1, String apellido2, 
-            String user, String pass, String id) {
+            String tipo, String user, String pass, String id) {
         
         nombreUno = nombre1;
         nombreDos = nombre2;
@@ -29,6 +30,7 @@ public class Usuario {
         apellidoDos = apellido2;
         username = user;
         password = pass;
+        tipoIdentificacion = tipo;
         identificacion = id;
         estado = false;
         empleo = true;
@@ -39,6 +41,42 @@ public class Usuario {
     public void editarNombreUno (String nombre) {
         
         nombreUno = nombre;
+        
+    }
+    
+    public void editarNombreDos (String nombre) {
+        
+        nombreDos = nombre;
+        
+    }
+    
+    public void editarApellidoUno (String apellido) {
+        
+        apellidoUno = apellido;
+        
+    }
+    
+    public void editarApellidoDos (String apellido) {
+        
+        apellidoDos = apellido;
+        
+    }
+    
+    public void editarPassword (String contraseña) {
+        
+        password = contraseña;
+        
+    }    
+    
+    public void setEstado (boolean estado_sistema) {
+        
+        estado = estado_sistema;
+        
+    }
+    
+    public void setEmpleado (boolean estado_empleo) {
+        
+        empleo = estado_empleo;
         
     }
     
