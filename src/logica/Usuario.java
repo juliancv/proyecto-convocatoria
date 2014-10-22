40889cf617a00ccc233ad658818bef82432b9083
Anderson Enriquez <anderson.enriquez@correounivalle.edu.co>
2014-10-22 09:42:28 -0500

    Creacion clase usuario

--- src/logica/Usuario.java
+++ src/logica/Usuario.java
@@ -10,4 +10,36 @@ package logica;
  */
 public class Usuario {
     
+    private String nombreUno;
+    private String nombreDos;
+    private String apellidoUno;
+    private String apellidoDos;
+    private String username;
+    private String password;
+    private String identificacion;
+    private boolean estado;
+    private boolean empleo;
+
+    public Usuario(String nombre1, String nombre2, String apellido1, String apellido2, 
+            String user, String pass, String id) {
+        
+        nombreUno = nombre1;
+        nombreDos = nombre2;
+        apellidoUno = apellido1;
+        apellidoDos = apellido2;
+        username = user;
+        password = pass;
+        identificacion = id;
+        estado = false;
+        empleo = true;
+        
+    }
+    
+    
+    public void editarNombreUno (String nombre) {
+        
+        nombreUno = nombre;
+        
+    }
+    
 }
