fd7891aec1288202c796ff9bd119086e9c21f353
KellysSanta <kellys.santa@correounivalle.edu.co>
2014-10-28 07:07:16 -0500

    ventana usuario supervisor

---
+++ src/Gui/VentanaIniSupervisor1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Gui;
+
+import java.awt.*;
+import javax.swing.*;
+
+ public class VentanaIniSupervisor1 {
+
+    private Container cont;
+    private JPanel inicioSupervisor;
+    private JLabel encabezado;
+    private JButton informe, genero, ciudad, jornada, mejores, consultar, total, cerrar;
+    
+    public VentanaIniSupervisor1(){
+        
+    }
+    
+
+}
